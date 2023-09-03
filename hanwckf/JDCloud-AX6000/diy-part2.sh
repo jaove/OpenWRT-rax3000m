ae538d21f195ad9292f1cbbfe7372752dc853825
lgs2007m <116912542@qq.com>
2023-09-03 16:25:57 +0800

    Update diy-part2.sh

--- hanwckf/JDCloud-AX6000/diy-part2.sh
+++ hanwckf/JDCloud-AX6000/diy-part2.sh
@@ -19,4 +19,6 @@ chmod +x /tmp/clash >/dev/null 2>&1
 mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
 mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
 rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
-##---------------------------------------------------------
+##-----------------Set golang version down to 1.19.10 for docker building-----------------
+sed -i '/.*GO_VERSION_PATCH:=*/c\GO_VERSION_PATCH:=10' feeds/packages/lang/golang/golang/Makefile
+sed -i '/.*PKG_HASH:=*/c\PKG_HASH:=13755bcce529747d5f2930dee034730c86d02bd3e521ab3e2bbede548d3b953f' feeds/packages/lang/golang/golang/Makefile
